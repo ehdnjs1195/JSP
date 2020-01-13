ea93dfa9ab1207e813a6545cc5311429d1f9f813
ADW <ehdnjs1195@naver.com>
2020-01-13 15:59:00 +0900

    [Acorn] 회원 삭제기능 구현함.

--- Step05_Login/src/test/users/dao/UsersDao.java
+++ Step05_Login/src/test/users/dao/UsersDao.java
@@ -18,6 +18,37 @@ public class UsersDao {	//싱글톤
 		}
 		return dao;
 	}
+	//회원가입 정보를 삭제하는 메소드
+	public boolean delete(String id) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int flag = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "delete from users"
+					+ " where id=?";
+			pstmt = conn.prepareStatement(sql);
+			//?에 값 바인딩하기
+			pstmt.setString(1, id);
+			flag = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (flag > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 	//비밀번호를 수정 반영하는 메소드
 	public boolean updatePwd(UsersDto dto) {
 		Connection conn = null;
