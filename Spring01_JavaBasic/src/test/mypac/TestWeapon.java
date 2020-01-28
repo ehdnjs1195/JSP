3bbe5d46f0027580806b061f5e9dd76acc023859
ADW <ehdnjs1195@naver.com>
2020-01-28 13:11:38 +0900

    [Acorn] Spring 예제 이동.

--- Spring01_JavaBasic/src/test/mypac/TestWeapon.java
+++
@@ -1,8 +0,0 @@
-package test.mypac;
-
-public class TestWeapon implements Weapon{
-	@Override
-	public void attack() {
-		System.out.println("반장님을 공격했어요!!엄청 씨게!!근데 튜브에 막혔어요...");
-	}
-}
--- Spring01_JavaBasic/src/test/mypac/WeaponImpl.java
+++
@@ -1,9 +0,0 @@
-package test.mypac;
-
-public class WeaponImpl implements Weapon{
-	
-	@Override
-	public void attack() {
-		System.out.println("귤을 까버려요!!");
-	}
-}
